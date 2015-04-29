cce6b3a84b278c34d69de70d213c614fbd28cbf9
aditya <aditya@Primus.Home>
2015-04-29 19:37:44 +0530

    Changes

--- Sort Analysis/src/SortMenu.c
+++ Sort Analysis/src/SortMenu.c
@@ -42,48 +42,59 @@ void callsort()
 	shs = genrandarr(size);
 	gs = genrandarr(size);
 	rs = genrandarr(size);
+	struct node *head = (struct node*) malloc(sizeof(struct node));
+	genrandlist(head, size);
+
 	*(start + 0) = clock();
 	QuickSort(qs, 0, size);
 	*(end + 0) = clock();
 	*(cpu_time_used + 0) = ((double) (*(end + 0) - *(start + 0))) / CLOCKS_PER_SEC;
-	struct node *head = NULL;
-	genrandlist(head, size);
+
 	*(start + 1) = clock();
 	MergeSort(head);
 	*(end + 1) = clock();
 	*(cpu_time_used + 1) = ((double) (*(end + 1) - *(start + 1))) / CLOCKS_PER_SEC;
+
 	*(start + 2) = clock();
 	HeapSort(hs, size);
 	*(end + 2) = clock();
 	*(cpu_time_used + 2) = ((double) (*(end + 2) - *(start + 2))) / CLOCKS_PER_SEC;
+
 	*(start + 3) = clock();
 	InsertionSort(is, size);
 	*(end + 3) = clock();
 	*(cpu_time_used + 3) = ((double) (*(end + 3) - *(start + 3))) / CLOCKS_PER_SEC;
+
 	*(start + 4) = clock();
 	SelectionSort(ss, size);
 	*(end + 4) = clock();
 	*(cpu_time_used + 4) = ((double) (*(end + 4) - *(start + 4))) / CLOCKS_PER_SEC;
+
 	*(start + 5) = clock();
 	BubbleSort(bs, size);
 	*(end + 5) = clock();
 	*(cpu_time_used + 5) = ((double) (*(end + 5) - *(start + 5))) / CLOCKS_PER_SEC;
+
 	*(start + 6) = clock();
 	BucketSort(bucs, size);
 	*(end + 6) = clock();
 	*(cpu_time_used + 6) = ((double) (*(end + 6) - *(start + 6))) / CLOCKS_PER_SEC;
+
 	*(start + 7) = clock();
 	CombSort(cs, size);
 	*(end + 7) = clock();
 	*(cpu_time_used + 7) = ((double) (*(end + 7) - *(start + 7))) / CLOCKS_PER_SEC;
+
 	*(start + 8) = clock();
 	ShellSort(shs, size);
 	*(end + 8) = clock();
 	*(cpu_time_used + 8) = ((double) (*(end + 8) - *(start + 8))) / CLOCKS_PER_SEC;
+
 	*(start + 9) = clock();
 	GnomeSort(gs, size);
 	*(end + 9) = clock();
 	*(cpu_time_used + 9) = ((double) (*(end + 9) - *(start + 9))) / CLOCKS_PER_SEC;
+
 	*(start + 10) = clock();
 	RadixSort(rs, size);
 	*(end + 10) = clock();
