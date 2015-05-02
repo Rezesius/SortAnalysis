6747d7889e8e5e1074e2600ba019af80a8e5e538
Rezesius <Adityamenon5@gmail.com>
2015-05-02 15:37:46 +0530

    Bug fixes.

--- Sort Analysis/src/HeapSort.c
+++ Sort Analysis/src/HeapSort.c
@@ -5,43 +5,77 @@
  *      Author: aditya
  */
 
-void HeapSort(int *array, int size)
+#include <stdlib.h>
+#include "Swap.h"
+
+// A heap has current size and array of elements
+struct MaxHeap
 {
-	int i, j, c, root, temp;
-	for (i = 1; i < size; i++)
-	{
-		c = i;
-	    do
-	    {
-	    	root = (c - 1) / 2;
-	        if (*(array + root) < *(array + c))   /* to create MAX heap array */
-	        {
-	        	temp = *(array + root);
-	        	*(array + root) = *(array + c);
-	        	*(array + c) = temp;
-	        }
-	        c = root;
-	    } while (c != 0);
-	}
-	for (j = size - 1; j >= 0; j--)
-	{
-		temp = *(array + 0);
-		*(array + 0) = *(array + j);    /* swap max element with rightmost leaf element */
-		*(array + j) = temp;
-	    root = 0;
-	    do
-	    {
-	    	c = 2 * root + 1;    /* left node of root element */
-	        if ((*(array + c) < *(array + c + 1)) && c < j - 1)
-	        	c++;
-	        if (*(array + root) < *(array + c) && c < j)    /* again rearrange to max heap array */
-	        {
-	        	temp = *(array + root);
-	        	*(array + root) = *(array + c);
-	        	*(array + c) = temp;
-	        }
-	        root = c;
-	    } while (c < j);
-	}
+    int size;
+    int* array;
+};
+
+// The main function to heapify a Max Heap. The function
+// assumes that everything under given root (element at
+// index idx) is already heapified
+void maxHeapify(struct MaxHeap* maxHeap, int idx)
+{
+    int largest = idx;  // Initialize largest as root
+    int left = (idx << 1) + 1;  // left = 2*idx + 1
+    int right = (idx + 1) << 1; // right = 2*idx + 2
+
+    // See if left child of root exists and is greater than
+    // root
+    if (left < maxHeap->size &&
+        maxHeap->array[left] > maxHeap->array[largest])
+        largest = left;
+
+    // See if right child of root exists and is greater than
+    // the largest so far
+    if (right < maxHeap->size &&
+        maxHeap->array[right] > maxHeap->array[largest])
+        largest = right;
+
+    // Change root, if needed
+    if (largest != idx)
+    {
+        swap(&maxHeap->array[largest], &maxHeap->array[idx]);
+        maxHeapify(maxHeap, largest);
+    }
 }
 
+// A utility function to create a max heap of given capacity
+struct MaxHeap* createAndBuildHeap(int *array, int size)
+{
+    int i;
+    struct MaxHeap* maxHeap = (struct MaxHeap*) malloc(sizeof(struct MaxHeap));
+    maxHeap->size = size;   // initialize size of heap
+    maxHeap->array = array; // Assign address of first element of array
+
+    // Start from bottom most and rightmost internal mode and heapify all
+    // internal modes in bottom up way
+    for (i = (maxHeap->size - 2) / 2; i >= 0; --i)
+        maxHeapify(maxHeap, i);
+    return maxHeap;
+}
+
+// The main function to sort an array of given size
+void HeapSort(int* array, int size)
+{
+    // Build a heap from the input data.
+    struct MaxHeap* maxHeap = createAndBuildHeap(array, size);
+
+    // Repeat following steps while heap size is greater than 1.
+    // The last element in max heap will be the minimum element
+    while (maxHeap->size > 1)
+    {
+        // The largest item in Heap is stored at the root. Replace
+        // it with the last item of the heap followed by reducing the
+        // size of heap by 1.
+        swap(&maxHeap->array[0], &maxHeap->array[maxHeap->size - 1]);
+        --maxHeap->size;  // Reduce heap size
+
+        // Finally, heapify the root of tree.
+        maxHeapify(maxHeap, 0);
+    }
+}
